a94696d7002f174b99f0c8eb5e8ca94cf3c92d9d
Simon250388 <A.v.simonov@mail.ru>
2020-02-05 11:09:22 +0400

    fix script

--- script/build.ps1
+++ script/build.ps1
@@ -4,10 +4,12 @@ $PlatformPath = $configFile.configuration.Platform1c.add | Where { $_.Key -eq 'p
 $appName =  "$PlatformPath\bin\1cv8.exe"
 $parentPath = (Split-Path $curentPath -Parent)
 $template = "$parentPath\template\step_definitions.xml"
-Get-ChildItem -Path $parentPath -Include *.feature -Exclude example.feature -Recurse | foreach { 
+$Excludes = '\\export\\|\\template\\'
+Get-ChildItem -Path $parentPath -Include *.feature -Recurse | where { $_.FullName -notmatch $Excludes} | foreach { 
     $featurePath = $_.Name -replace ".feature",".epf"
     $directory = $_.Directory.FullName 
     $epfPath  = ("$directory\step_definitions\$featurePath")
     if ((Test-Path  $epfPath  ) -eq "True"){
         Remove-Item -Path $epfPath}
+    write-host  $epfPath    
     & $appName DESIGNER /LoadExternalDataProcessorOrReportFromFiles $template $epfPath  | Write-Host -NoNewline }
