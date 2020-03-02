23e5451bd3096550ef6173d417a07c6a69409e08
Simon250388 <A.v.simonov@mail.ru>
2020-03-02 17:10:46 +0400

    fix

--- script/build.ps1
+++ script/build.ps1
@@ -1,11 +1,17 @@
 $curentPath = (Split-Path $MyInvocation.MyCommand.Path -Parent)
 [xml]$configFile = get-content $curentPath\Config.xml
 $PlatformPath = $configFile.configuration.Platform1c.add | Where-Object { $_.Key -eq 'path' } | ForEach-Object { $_.value }
+$ProjectFolder = $configFile.configuration.buildFolder.add | Where-Object { $_.Key -eq 'path' } | ForEach-Object { $_.value }
 $appName = "$PlatformPath\bin\1cv8.exe"
 $parentPath = (Split-Path $curentPath -Parent)
 $template = "$parentPath\tmp_template\step_definitions.xml"
 $Excludes = '\\export\\|\\template\\'
-$ObjectNames = Get-ChildItem -Path $parentPath -Include *.feature -Recurse | Where-Object { $_.FullName -notmatch $Excludes } 
+if ([string]::IsNullOrEmpty($ProjectFolder)) {
+    $ObjectNames = Get-ChildItem -Path $parentPath -Include *.feature -Recurse | Where-Object { $_.FullName -notmatch $Excludes } 
+}
+else {
+    $ObjectNames = Get-ChildItem -Path ("$parentPath\$ProjectFolder") -Include *.feature -Recurse | Where-Object { $_.FullName -notmatch $Excludes } 
+}
 $ObjectNames | ForEach-Object {
     Copy-Item -Path ("$parentPath\template\*") -Destination ("$parentPath\tmp_template\") -Recurse -Force
     $relativePath = (Split-Path (Resolve-Path $_.FullName -Relative) -Parent)
