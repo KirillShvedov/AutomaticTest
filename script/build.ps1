7309754e81e2433e7172494b20a4b5dd40c1965a
Simanov Aleksey <a.v.simonov@mail.ru>
2020-05-25 21:56:21 +0400

    fix build

--- script/build.ps1
+++ script/build.ps1
@@ -1,35 +1,38 @@
 $curentPath = (Split-Path $MyInvocation.MyCommand.Path -Parent)
 [xml]$configFile = get-content $curentPath\Config.xml
 $PlatformPath = $configFile.configuration.Platform1c.add | Where-Object { $_.Key -eq 'path' } | ForEach-Object { $_.value }
-$ProjectFolder = $configFile.configuration.buildFolder.add | Where-Object { $_.Key -eq 'path' } | ForEach-Object { $_.value }
+
 $appName = "$PlatformPath\bin\1cv8.exe"
 $parentPath = (Split-Path $curentPath -Parent)
 $template = "$parentPath\tmp_template\step_definitions.xml"
 $Excludes = '\\export\\|\\template\\'
-if ([string]::IsNullOrEmpty($ProjectFolder)) {
-    $ObjectNames = Get-ChildItem -Path $parentPath -Include *.feature -Recurse | Where-Object { $_.FullName -notmatch $Excludes } 
-}
-else {
-    $ObjectNames = Get-ChildItem -Path ("$parentPath\$ProjectFolder") -Include *.feature -Recurse | Where-Object { $_.FullName -notmatch $Excludes } 
-}
-$ObjectNames | ForEach-Object {
-    Copy-Item -Path ("$parentPath\template\*") -Destination ("$parentPath\tmp_template\") -Recurse -Force
-    $relativePath = (Split-Path (Resolve-Path $_.FullName -Relative) -Parent)
-    $featureName = $_.Name -replace ".feature", ""
-    Get-ChildItem -Path ".\tmp_template\" -Recurse | Where-Object { $_.PsIsContainer -eq $false } | ForEach-Object { Set-Content -Path $_.FullName -Value (((Get-Content $_.FullName ) -replace "relative_step_definitions", $relativePath) -replace "step_definitions", $featureName) -Encoding "UTF8" }
-    $featurePath = $_.Name -replace ".feature", ".epf"
-    $directory = $_.Directory.FullName 
-    $epfPath = ("$directory\step_definitions\$featurePath")
-    if ((Test-Path  $epfPath  ) -eq "True") {
-        Remove-Item -Path $epfPath | write-host -NoNewline 
-    }    
-    [string[]]$argList = "DESIGNER", "/LoadExternalDataProcessorOrReportFromFiles", """$template""", """$epfPath"""
-    Start-Process -FilePath $appName -ArgumentList $argList -NoNewWindow -Wait
-    while ($true) {
-        if ((Test-Path  $epfPath  ) -eq "True") {
-            break;
-        }
-        Start-Process -FilePath $appName -ArgumentList $argList -NoNewWindow -Wait
+
+$configFile.configuration.buildFolder.add | Where-Object { $_.Key -eq 'path' } | ForEach-Object {
+    $ProjectFolder = $_.value 
+    if ([string]::IsNullOrEmpty($ProjectFolder)) {
+        $ObjectNames = Get-ChildItem -Path $parentPath -Include *.feature -Recurse | Where-Object { $_.FullName -notmatch $Excludes } 
     }
-    write-host  $epfPath
-    Remove-Item -Path ("$parentPath\tmp_template\*") -Recurse | write-host -NoNewline }
+    else {
+        $ObjectNames = Get-ChildItem -Path ("$parentPath\$ProjectFolder") -Include *.feature -Recurse | Where-Object { $_.FullName -notmatch $Excludes } 
+    }
+    $ObjectNames | ForEach-Object {
+        Copy-Item -Path ("$parentPath\template\*") -Destination ("$parentPath\tmp_template\") -Recurse -Force
+        $relativePath = (Split-Path (Resolve-Path $_.FullName -Relative) -Parent)
+        $featureName = $_.Name -replace ".feature", ""
+        Get-ChildItem -Path ".\tmp_template\" -Recurse | Where-Object { $_.PsIsContainer -eq $false } | ForEach-Object { Set-Content -Path $_.FullName -Value (((Get-Content $_.FullName ) -replace "relative_step_definitions", $relativePath) -replace "step_definitions", $featureName) -Encoding "UTF8" }
+        $featurePath = $_.Name -replace ".feature", ".epf"
+        $directory = $_.Directory.FullName 
+        $epfPath = ("$directory\step_definitions\$featurePath")
+        if ((Test-Path  $epfPath  ) -eq "True") {
+            Remove-Item -Path $epfPath | write-host -NoNewline 
+        }    
+        [string[]]$argList = "DESIGNER", "/LoadExternalDataProcessorOrReportFromFiles", """$template""", """$epfPath"""
+        Start-Process -FilePath $appName -ArgumentList $argList -NoNewWindow -Wait
+        while ($true) {
+            if ((Test-Path  $epfPath  ) -eq "True") {
+                break;
+            }
+            Start-Process -FilePath $appName -ArgumentList $argList -NoNewWindow -Wait
+        }
+        write-host  $epfPath
+        Remove-Item -Path ("$parentPath\tmp_template\*") -Recurse | write-host -NoNewline } }
